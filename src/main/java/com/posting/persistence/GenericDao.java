d932dba47be64f151a1e6ac1e68fd294bc6df6ef
nateP82 <npeck@madisoncollege.edu>
2023-03-29 15:23:20 -0500

    removing unused import statements

--- src/main/java/com/posting/persistence/GenericDao.java
+++ src/main/java/com/posting/persistence/GenericDao.java
@@ -3,11 +3,9 @@ package com.posting.persistence;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.Root;
 import java.util.List;
 
