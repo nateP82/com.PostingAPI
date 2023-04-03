40aac2cf7db7117df114d3b7bf2abee8b7c3734e
nateP82 <npeck@madisoncollege.edu>
2023-04-03 13:55:40 -0500

    adding javadoc and webservlet url pattern

--- src/main/java/com/posting/controller/AccessManager.java
+++ src/main/java/com/posting/controller/AccessManager.java
@@ -1,10 +1,19 @@
 package com.posting.controller;
 
 import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+/**
+ * AccessManager web servlet that interacts with our services endpoint
+ * @author npeck
+ */
+@WebServlet (
+        urlPatterns = {"/accessServices"}
+)
 public class AccessManager extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
