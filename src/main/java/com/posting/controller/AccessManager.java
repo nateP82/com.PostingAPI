e9a468671f6a87ffb63d51f7ce1cb1d4333197bd
nateP82 <npeck@madisoncollege.edu>
2023-04-03 13:49:54 -0500

    adding AccessManager class with empty doGet and doPost methods

---
+++ src/main/java/com/posting/controller/AccessManager.java
@@ -0,0 +1,22 @@
+package com.posting.controller;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+public class AccessManager extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+
+    }
+
+
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+
+    }
+}
